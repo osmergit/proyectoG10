c94961f173d0914dea081009aea7faa1cb282ddd
osmergit <osmer1976@gmail.com>
2022-10-27 07:17:43 -0500

    Bienvenidos al Espacio

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.json())
 
 
 app.get('/',(req,res)=>{
-    res.send('😎HOLA MUNDO DESDE DOCKER 2022 😍')
+    res.send('😎HOLA MUNDO DESDE 🚀🚀🚀 DOCKER 2022 😍')
 }) 
 
 app.listen(8000, ()=> {
